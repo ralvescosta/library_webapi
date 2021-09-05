79d6e967d2fd416b5b02ff6fde7f40a50c28f738
ralvescosta <rafael.rac.mg@gmail.com>
2021-09-05 07:45:14 -0300

    refactor: remove some unused files

--- business/src/dtos/something_dto.rs
+++
@@ -1,6 +0,0 @@
-pub struct SomethingDto<'a> {
-    pub first_name: &'a str,
-    pub last_name: &'a str,
-    pub alias: &'a str,
-    pub email: &'a str,
-}
--- business/src/usecases/i_something.rs
+++
@@ -1,5 +0,0 @@
-use crate::dtos::something_dto::SomethingDto;
-
-pub trait ISomethingUseCase {
-    fn perform(&self, dto: SomethingDto);
-}
