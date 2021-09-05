849c659c785ea98c27ababc0d90d332e392d0861
ralvescosta <rafael.rac.mg@gmail.com>
2021-09-05 07:45:52 -0300

    refactor: book repository

--- infrastructure/src/repositories/book_repository.rs
+++ infrastructure/src/repositories/book_repository.rs
@@ -1,16 +1,21 @@
+use std::sync::Arc;
+use application::interfaces::i_logger::ILogger;
 use application::interfaces::i_book_repository::IBookRepository;
 use business::entities::book::Book;
 
-pub struct BookRepository {}
+pub struct BookRepository {
+    logger: Arc<dyn ILogger>
+}
 
 impl IBookRepository for BookRepository {
     fn create(&self) -> Book {
-        todo!()
+        self.logger.info("BookRepository".to_string());
+        Book{}
     }
 }
 
 impl BookRepository {
-    pub fn new() -> Box<BookRepository> {
-        Box::new(BookRepository {})
+    pub fn new(logger: Arc<dyn ILogger>) -> BookRepository {
+        BookRepository {logger}
     }
 }
