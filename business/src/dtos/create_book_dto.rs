71e311c9374e9440f9e19eb8e76059eb6ad8b79f
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-07 05:24:48 -0300

    feat: book entity

--- business/src/dtos/create_book_dto.rs
+++ business/src/dtos/create_book_dto.rs
@@ -1,7 +1,8 @@
+#[derive(Clone)]
 pub struct CreateBookDto {
-  pub title: String,
-  pub subject: String,
-  pub author: String,
-  pub published_data: String,
-  pub editor: String
+    pub title: String,
+    pub subject: String,
+    pub author: String,
+    pub published_data: String,
+    pub editor: String,
 }
--- business/src/entities/book.rs
+++ business/src/entities/book.rs
@@ -1,1 +1,10 @@
-pub struct Book {}
+pub struct Book {
+    pub title: String,
+    pub subject: String,
+    pub author: String,
+    pub published_date: Option<String>,
+    pub editor: String,
+    pub created_at: String,
+    pub updated_at: String,
+    pub deleted_at: Option<String>,
+}
