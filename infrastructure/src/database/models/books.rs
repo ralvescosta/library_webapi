8f1f1957125e0347ced93f76aeab9d1f7e6a2d01
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-08 07:37:21 -0300

    refactor: book model

--- infrastructure/src/database/models/books.rs
+++ infrastructure/src/database/models/books.rs
@@ -1,4 +1,5 @@
 use crate::schema::books;
+use business::{dtos::create_book_dto::CreateBookDto, entities::book::Book};
 use chrono::{DateTime, Utc};
 
 #[derive(Insertable)]
@@ -9,8 +10,18 @@ pub struct CreateBookModel {
     pub author: String,
     pub published_date: Option<DateTime<Utc>>,
     pub editor: String,
-    pub created_at: DateTime<Utc>,
-    pub updated_at: DateTime<Utc>,
+}
+
+impl CreateBookModel {
+    pub fn from_create_book_dto(dto: CreateBookDto) -> CreateBookModel {
+        CreateBookModel {
+            title: dto.title,
+            subject: dto.subject,
+            author: dto.author,
+            published_date: None,
+            editor: dto.editor,
+        }
+    }
 }
 
 #[derive(Debug, Queryable)]
@@ -24,4 +35,28 @@ pub struct BookModel {
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
     pub deleted_at: Option<DateTime<Utc>>,
+}
+
+impl BookModel {
+    pub fn to_book(self) -> Book {
+        let published_date = match self.published_date {
+            Some(date) => Some(date.to_string()),
+            None => None,
+        };
+        let deleted_at = match self.deleted_at {
+            Some(date) => Some(date.to_string()),
+            None => None,
+        };
+        Book {
+            id: self.id,
+            title: self.title,
+            subject: self.subject,
+            author: self.author,
+            published_date,
+            editor: self.editor,
+            created_at: self.created_at.to_string(),
+            updated_at: self.updated_at.to_string(),
+            deleted_at,
+        }
+    }
 }
