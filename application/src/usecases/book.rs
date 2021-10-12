9078f3dfc07745bb0d88ba132d3047d34a8badc9
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-12 11:04:19 -0300

    refactor: book usecase

--- application/src/usecases/book.rs
+++ application/src/usecases/book.rs
@@ -2,14 +2,16 @@ use std::sync::Arc;
 
 use crate::interfaces::{i_book_repository::IBookRepository, i_logger::ILogger};
 use business::dtos::create_book_dto::CreateBookDto;
+use business::dtos::update_book_dto::UpdateBookDto;
 use business::entities::book::Book;
-use business::usecases::i_book::ICreateBookUseCase;
+use business::usecases::i_book::{
+    ICreateBookUseCase, IDeleteBookUseCase, IGetBookUseCase, IUpdateBookUseCase,
+};
 
 pub struct CreateBookUseCase {
     logger: Arc<dyn ILogger>,
     repository: Arc<dyn IBookRepository>,
 }
-
 impl ICreateBookUseCase for CreateBookUseCase {
     fn perform(&self, dto: CreateBookDto) -> Result<Book, ()> {
         self.logger.info("IBookUseCase", "Logger");
@@ -26,5 +28,56 @@ impl CreateBookUseCase {
         repository: Arc<dyn IBookRepository>,
     ) -> impl ICreateBookUseCase {
         CreateBookUseCase { logger, repository }
+    }
+}
+
+pub struct GetBookUseCase {
+    repository: Arc<dyn IBookRepository>,
+}
+impl IGetBookUseCase for GetBookUseCase {
+    fn perform(&self, id: i32) -> Result<Option<Book>, ()> {
+        match self.repository.get_by_id(id) {
+            Ok(book) => Ok(book),
+            _ => Err(()),
+        }
+    }
+}
+impl GetBookUseCase {
+    pub fn new(repository: Arc<dyn IBookRepository>) -> impl IGetBookUseCase {
+        GetBookUseCase { repository }
+    }
+}
+
+pub struct UpdateBookUseCase {
+    repository: Arc<dyn IBookRepository>,
+}
+impl IUpdateBookUseCase for UpdateBookUseCase {
+    fn perform(&self, id: i32, dto: UpdateBookDto) -> Result<bool, ()> {
+        match self.repository.update(id, dto) {
+            Ok(result) => Ok(result),
+            _ => Err(()),
+        }
+    }
+}
+impl UpdateBookUseCase {
+    pub fn new(repository: Arc<dyn IBookRepository>) -> impl IUpdateBookUseCase {
+        UpdateBookUseCase { repository }
+    }
+}
+
+pub struct DeleteBookUseCase {
+    repository: Arc<dyn IBookRepository>,
+}
+impl IDeleteBookUseCase for DeleteBookUseCase {
+    fn perform(&self, id: i32) -> Result<bool, ()> {
+        match self.repository.delete_by_id(id) {
+            Ok(result) => Ok(result),
+            _ => Err(()),
+        }
+    }
+}
+impl DeleteBookUseCase {
+    pub fn new(repository: Arc<dyn IBookRepository>) -> impl IDeleteBookUseCase {
+        DeleteBookUseCase { repository }
     }
 }
--- business/src/usecases/i_book.rs
+++ business/src/usecases/i_book.rs
@@ -1,5 +1,20 @@
-use crate::{dtos::create_book_dto::CreateBookDto, entities::book::Book};
+use crate::{
+    dtos::{create_book_dto::CreateBookDto, update_book_dto::UpdateBookDto},
+    entities::book::Book,
+};
 
 pub trait ICreateBookUseCase {
     fn perform(&self, dto: CreateBookDto) -> Result<Book, ()>;
+}
+
+pub trait IGetBookUseCase {
+    fn perform(&self, id: i32) -> Result<Option<Book>, ()>;
+}
+
+pub trait IUpdateBookUseCase {
+    fn perform(&self, id: i32, dto: UpdateBookDto) -> Result<bool, ()>;
+}
+
+pub trait IDeleteBookUseCase {
+    fn perform(&self, id: i32) -> Result<bool, ()>;
 }
